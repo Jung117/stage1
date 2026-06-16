6a7b20a6a54322d2093a534ad1835f522ababdf9
juggle-tech <chiajung.hsu17@gmail.com>
2026-06-16 11:01:41 +0900

    Update partical code for Task 2 and add exit early condition for Task 1.

--- week2/assignment2.py
+++ week2/assignment2.py
@@ -1,58 +1,94 @@
 ## Task 1
-def func1(name): 
-    # [x-axis, y-axis, right/left ]: left side = 2; right side = 0
-    characters = {"悟空": [0, 0, 2], "辛巴": [-3, 3, 2], "丁滿": [-1, 4, 0], 
-                  "貝吉塔": [-4, -1, 2], "特南克斯": [1, -2, 2], "弗利沙": [4, -1, 0]}
-    closest = []
-    farthest = []
-    min, max = 0, 0
-
-    for char in characters:
-        if char is not name:
-            
-            # Calculate distance from name to char  
-            distance = (abs(characters[name][0] - characters[char][0]) 
-                            + abs(characters[name][1] - characters[char][1]) 
-                                + abs(characters[name][2] - characters[char][2]))
-
-            # Update max if applicable
-            if distance > max:
-                farthest = [char]
-                max = distance
-            elif distance is max:
-                farthest += [char]
-
-            # Update min if applicable
-            if distance < min:
-                closest = [char]
-                min = distance
-            elif distance is min or min is 0:
-                closest += [char]
-                min = distance
+# def func1(name): 
     
-    print(f"最遠{'、'.join(farthest)}；最近{'、'.join(closest)}")
+#     # [x-axis, y-axis, right/left ]: left side = 2; right side = 0
+#     characters = {"悟空": [0, 0, 2], "辛巴": [-3, 3, 2], "丁滿": [-1, 4, 0], 
+#                   "貝吉塔": [-4, -1, 2], "特南克斯": [1, -2, 2], "弗利沙": [4, -1, 0]}
+    
+#     # Exit early if input character is not found
+#     if name not in characters:
+#         print(f"Character {name} not found.")
+#         return
+    
+#     closest = []
+#     farthest = []
+#     min, max = 0, 0
 
-func1("辛巴") # print 最遠弗利沙；最近丁滿、貝吉塔 
-func1("悟空") # print 最遠丁滿、弗利沙；最近特南克斯 
-func1("弗利沙") # print 最遠辛巴，最近特南克斯 
-func1("特南克斯") # print 最遠丁滿，最近悟空
+#     for char in characters:
+#         if char != name:
+            
+#             # Calculate distance from name to char  
+#             distance = (abs(characters[name][0] - characters[char][0]) 
+#                             + abs(characters[name][1] - characters[char][1]) 
+#                                 + abs(characters[name][2] - characters[char][2]))
+
+#             # Update max if applicable
+#             if distance > max:
+#                 farthest = [char]
+#                 max = distance
+#             elif distance is max:
+#                 farthest += [char]
+
+#             # Update min if applicable
+#             if distance < min:
+#                 closest = [char]
+#                 min = distance
+#             elif distance is min or min is 0:
+#                 closest += [char]
+#                 min = distance
+    
+#     print(f"最遠{'、'.join(farthest)}；最近{'、'.join(closest)}")
+
+# func1("辛巴") # print 最遠弗利沙；最近丁滿、貝吉塔 
+# func1("悟空") # print 最遠丁滿、弗利沙；最近特南克斯 
+# func1("弗利沙") # print 最遠辛巴，最近特南克斯 
+# func1("特南克斯") # print 最遠丁滿，最近悟空
 
 
 
 
 ## Task 2
-# your code here, maybe 
+booking = {}
 def func2(ss, start, end, criteria): 
-    # your code here 
-    services=[ 
-        {"name":"S1", "r":4.5, "c":1000}, 
-        {"name":"S2", "r":3, "c":1200}, 
-        {"name":"S3", "r":3.8, "c":800}  
-    ] 
+
+    # Terminate early if no services are available
+    if not services:
+        print("No services available.")
+        return
+
+    # Create time slots for each service throughout the day: available = 0; taken = 1
+    if not booking:
+        for service in services:
+            booking[service["name"]] = [0]*24
+            
+    
+    closest = ""
+    min = float('inf')
+
+    if criteria[0] == "c" and criteria[1] in (">", "=", "<"):
+        for service in services:
+            pass
+
+    elif criteria[0] == "r" and criteria[1] in (">", "=", "<"):
+        for service in services:
+            pass
+    elif criteria[:4] == "name" and criteria[4] == "=":
+        for service in services:
+            pass
+    else:
+        print(f"Invalid input for criteria {criteria}.")
+
+    
+
+services=[ 
+    {"name":"S1", "r":4.5, "c":1000}, 
+    {"name":"S2", "r":3, "c":1200}, 
+    {"name":"S3", "r":3.8, "c":800}  
+] 
 
 # func2(services, 15, 17, "c>=800") # S3 
 # func2(services, 11, 13, "r<=4") # S3 
-# func2(services, 10, 12, "name=S3") # Sorry 
+func2(services, 10, 12, "name=S3") # Sorry 
 # func2(services, 15, 18, "r>=4.5") # S1 
 # func2(services, 16, 18, "r>=4") # Sorry 
 # func2(services, 13, 17, "name=S1") # Sorry 
