8c31e8a16e3689467bf811667644ef507336988e
juggle-tech <chiajung.hsu17@gmail.com>
2026-06-16 16:34:27 +0900

    Result check

--- week2/assignment2.py
+++ week2/assignment2.py
@@ -39,10 +39,12 @@ def func1(name):
     
     print(f"最遠{'、'.join(farthest)}；最近{'、'.join(closest)}")
 
-# func1("辛巴") # print 最遠弗利沙；最近丁滿、貝吉塔 
-# func1("悟空") # print 最遠丁滿、弗利沙；最近特南克斯 
-# func1("弗利沙") # print 最遠辛巴，最近特南克斯 
-# func1("特南克斯") # print 最遠丁滿，最近悟空
+
+print()
+func1("辛巴") # print 最遠弗利沙；最近丁滿、貝吉塔 
+func1("悟空") # print 最遠丁滿、弗利沙；最近特南克斯 
+func1("弗利沙") # print 最遠辛巴，最近特南克斯 
+func1("特南克斯") # print 最遠丁滿，最近悟空
 
 
 
@@ -191,15 +193,17 @@ services=[
     {"name":"S1", "r":4.5, "c":1000}, 
     {"name":"S2", "r":3, "c":1200}, 
     {"name":"S3", "r":3.8, "c":800}  
-] 
+]
 
-# func2(services, 15, 17, "c>=800") # S3
-# func2(services, 11, 13, "r<=4") # S3
-# func2(services, 10, 12, "name=S3") # Sorry 
-# func2(services, 15, 18, "r>=4.5") # S1 
-# func2(services, 16, 18, "r>=4") # Sorry 
-# func2(services, 13, 17, "name=S1") # Sorry 
-# func2(services, 8, 9, "c<=1500") # S2
+
+print()
+func2(services, 15, 17, "c>=800") # S3
+func2(services, 11, 13, "r<=4") # S3
+func2(services, 10, 12, "name=S3") # Sorry 
+func2(services, 15, 18, "r>=4.5") # S1 
+func2(services, 16, 18, "r>=4") # Sorry 
+func2(services, 13, 17, "name=S1") # Sorry 
+func2(services, 8, 9, "c<=1500") # S2
 
 
 
@@ -217,10 +221,12 @@ def func3(index):
             num += 2
     print(num)
 
-# func3(1) # print 23 
-# func3(5) # print 21 
-# func3(10) # print 16 
-# func3(30) # print 6
+
+print()
+func3(1) # print 23 
+func3(5) # print 21 
+func3(10) # print 16 
+func3(30) # print 6
 
 
 
@@ -247,6 +253,7 @@ def func4(sp, stat, n):
     print(car)
                 
 
+print()
 func4([3, 1, 5, 4, 3, 2], "101000", 2) # print 5
 func4([1, 0, 5, 1, 3], "10100", 4) # print 4
 func4([4, 6, 5, 8], "1000", 4) # print 2
