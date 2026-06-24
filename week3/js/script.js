73d6f963926f53cafd83ebd2993b6a7f07a02a3b
juggle-tech <chiajung.hsu17@gmail.com>
2026-06-24 20:45:09 +0800

    Finish Task 3 and Update HTML and CSS

--- week3/js/script.js
+++ week3/js/script.js
@@ -1,44 +1,66 @@
 // Pop-up menu
-// const hamMenu = document.getElementById('hamMenu');
-// const menu = document.getElementById('menu');
-// const overlay = document.getElementById('overlay');
-// const closeBtn = document.getElementById('closeBtn');
+const hamMenu = document.getElementById('hamMenu');
+const menu = document.getElementById('menu');
+const overlay = document.getElementById('overlay');
+const closeBtn = document.getElementById('closeBtn');
 
-// // Add additional attributes to menu and overlay on hamburger menu click
-// hamMenu.addEventListener('click', () => {
-//     menu.classList.add('menuOpen');
-//     overlay.classList.add('overlayActive');
-// });
+// Add additional attributes to menu and overlay on hamburger menu click
+hamMenu.addEventListener('click', () => {
+    menu.classList.add('menuOpen');
+    overlay.classList.add('overlayActive');
+});
 
-// // Close the popup menu by clicking the close icon
-// closeBtn.addEventListener('click', () => {
-//     menu.classList.remove('menuOpen');
-//     overlay.classList.remove('overlayActive');
-// });
+// Close the popup menu by clicking the close icon
+closeBtn.addEventListener('click', () => {
+    menu.classList.remove('menuOpen');
+    overlay.classList.remove('overlayActive');
+});
 
 
 
-// API
+// Task 3
 const attrInfo = await fetch("https://cwpeng.github.io/test/assignment-3-1");
 const attrPic = await fetch("https://cwpeng.github.io/test/assignment-3-2");
 
 const info = await attrInfo.json();
 const pic = await attrPic.json();
 
-const attrInfoPic = {};
+const attrInfoPic = new Map();
 
 // Fetch name for each attraction
-// info.rows.forEach(item => {
-//     console.log(item.serial);
-//     console.log(item.sname);
-// });
+info.rows.forEach(attr => {
+
+    attrInfoPic.set(attr.serial, [attr.sname]);
+});
 
 // Fetch picture for each attraction
-pic.rows.forEach(item => {
-    console.log(item.serial);
-    let firstImage = item.pics.match(/\/resources\/images\/[^\/]+\.jpg/)[0]
-    console.log(firstImage);
+pic.rows.forEach(attr => {
+    let firstImage = attr.pics.match(/\/resources\/images\/[^\/]+\.jpg/)[0];
+    attrInfoPic.get(attr.serial).push(pic.host + firstImage);
 });
+
+// Convert map into a list for rendering attraction info
+const attrList = [...attrInfoPic];
+
+// Render attraction info
+for (let i = 1; i < 14; i++) {
+    
+    if (i < 4) {
+        
+        let imgBlock = document.getElementById("img"+i);
+        imgBlock.src = attrList[i-1][1][1];
+        let title = imgBlock.nextElementSibling;
+        title.textContent = attrList[i-1][1][0];
+    } else {
+        console.log(i);
+        let divBlock = document.getElementById("div"+(i-3));
+        divBlock.style.backgroundImage =  `url('${attrList[i][1][1]}')`;
+        // console.log(divBlock);
+        console.log(attrList[i][1][1]);
+        let title = divBlock.children[1];
+        title.textContent = attrList[i-1][1][0];
+    }
+}
 
 
 
