e5c0ff94318a640d460cd07ff74fc5e8a7072ee2
juggle-tech <chiajung.hsu17@gmail.com>
2026-06-29 22:24:05 +0800

    Update Task1

--- week4/main.py
+++ week4/main.py
@@ -1,18 +1,46 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from urllib.parse import quote
 
 app = FastAPI()
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
 templates = Jinja2Templates(directory="templates")
 
-# Index.html
-@app.get("/", response_class=HTMLResponse)
-async def index_page(request: Request):
+
+# index.html
+@app.get("/")
+async def index(request: Request):
     return templates.TemplateResponse(
-        request=request, name="index.html", context={}
+        request = request, name = "index.html", context = {}
+    )
+
+
+@app.post("/login")
+async def login(request: Request, email, pwd):
+    
+        if email == "abc@abc.com" and pwd == "abc":
+            return RedirectResponse(url="/member")
+        else:
+            error_msg = quote("輸入錯誤")
+            return RedirectResponse(url = f"/ohoh?msg={error_msg}")
+        
+
+
+# member.html
+@app.get("/member")
+async def member(request: Request):
+    return templates.TemplateResponse(
+        request = request, name = "member.html", context = {}
+    )
+            
+
+
+# Logout.html
+@app.get("/logout")
+async def logout(request):
+    return templates.TemplateResponse(
+            "logout.html",
+            {"request": request}
     )
--- week4/static/js/script.js
+++ week4/static/js/script.js
@@ -1,1 +1,10 @@
-// 
+console.log(document.getElementById("loginForm"));
+// Alert dialog for checkbox
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+    const checkbox = document.getElementById("agree");
+
+    if (!checkbox.checked) {
+        e.preventDefault();
+        alert("請勾選同意條款");
+    }
+});
