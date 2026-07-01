5df134a54fbb3923375ee7467748c8ee5f40d782
juggle-tech <chiajung.hsu17@gmail.com>
2026-07-01 17:07:53 +0800

    Finish up Task 4

--- week4/main.py
+++ week4/main.py
@@ -66,12 +66,41 @@ async def logout(request: Request):
 # hotel.html
 @app.get("/hotel/{hotel_num}")
 async def get_hotel(request: Request, hotel_num: int):
+
+    # Get hotels data 
+    hotels_data = retrieve_hotels_data()
+
+    # Retrieve hotel info by its id
+    if hotel_num in hotels_data:
+        hotel_info = "、".join(hotels_data[hotel_num])
+    else:
+        hotel_info = "查詢不到相關資料"
+    return templates.TemplateResponse(
+        request = request, name = "hotel.html", context = {"hotel_info": hotel_info}
+    )
+
+
+
+# Cache for hotels' info
+hotels_cache = {}
+  
+def retrieve_hotels_data():
+    """ Retrieve and process hotel data and return a dict for all hotels
+
+    Returns:
+        dict: A dict storing hotel info in required format
+    """
+
+    global hotels_cache
+
+    if hotels_cache:
+        return hotels_cache
     
     # Sources of the hotel data
     src_cn = "https://resources-wehelp-taiwan-b986132eca78c0b5eeb736fc03240c2ff8b7116.gitlab.io/hotels-ch"
     src_eng = "https://resources-wehelp-taiwan-b986132eca78c0b5eeb736fc03240c2ff8b7116.gitlab.io/hotels-en"
 
-    # Proccess hotel data and extract to lists
+    # Process hotel data and extract into lists
     with req.urlopen(src_cn) as response:
             data_cn = json.load(response)
 
@@ -82,22 +111,12 @@ async def get_hotel(request: Request, hotel_num: int):
     h_list_eng = data_eng["list"]
 
     # Store required data into dict
-    hotels_data = {}
     for hotel in h_list_cn:
-        if hotel["_id"] not in hotels_data:
-            hotels_data[hotel["_id"]] = [hotel["旅宿名稱"]]
+        if hotel["_id"] not in hotels_cache:
+            hotels_cache[hotel["_id"]] = [hotel["旅宿名稱"]]
 
     for hotel in h_list_eng:
-        if hotel["_id"] in hotels_data:
-            hotels_data[hotel["_id"]] += [hotel["hotel name"], hotel["tel"]]
+        if hotel["_id"] in hotels_cache:
+            hotels_cache[hotel["_id"]] += [hotel["hotel name"], hotel["tel"]]
 
-
-    # Retrieve hotel info with it id
-    if hotel_num in hotels_data:
-        hotel_info = "、".join(hotels_data[hotel_num])
-    else:
-        hotel_info = "查詢不到相關資料"
-    return templates.TemplateResponse(
-        request = request, name = "hotel.html", context = {"hotel_info": hotel_info}
-    )
-    
+    return hotels_cache
