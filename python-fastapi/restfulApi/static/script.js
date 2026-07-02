dd3e31ee177636f88e6caf117aadc6a5f79c4219
juggle-tech <chiajung.hsu17@gmail.com>
2026-07-02 15:47:21 +0800

    Finsih restful api demo

--- python-fastapi/restfulApi/static/script.js
+++ python-fastapi/restfulApi/static/script.js
@@ -1,14 +1,19 @@
-
 // 新增留言
 async function postMessage() {
     let response = await fetch("/api/message", {
         method: "POST",
-        body: JSON.stringify({"author": "jung", "content": "Test"})
+        // 資料是寫死的，按按鈕後會存取下面的內容
+        body: JSON.stringify({
+            "author": "jung", 
+            "content": "Test"}
+        )
     });
-
+    
+    // 取得 {"OK": True} if success
     let result = await response.json();
     console.log(result);
 }
+
 
 // 取得留言
 async function getMessages() {
@@ -20,14 +25,13 @@ async function getMessages() {
     console.log(result);
 }
 
+
 // 刪除留言
 async function deleteMessage() {
-    let response = await fetch("/api/message", {
+    let response = await fetch("/api/message/2", {
         method: "DELETE"
     });
 
     let result = await response.json();
     console.log(result);
 }
-
-getMessages();
